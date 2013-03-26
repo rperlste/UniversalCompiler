71c212d7c27bebf4f2849e56590c9a14a27518d6
Robbie Perlstein <rperlste@gmail.com>
2013-03-25 23:32:50 -0600

    Removed zombie code.

--- ActionSymbols.h
+++ ActionSymbols.h
@@ -12,19 +12,5 @@ enum ActionSymbol{
 	ProcessId,
 	Copy
 };
-/*
-namespace ActionSymbols{
-
-	void Start();
-	void Assign( $1, $2 );
-	void ReadId( $1);
-	void WriteExpr( $1 );
-	void GenInfix( $$, $1, $2, $$ );
-	void ProcessLiteral();
-	void ProcessOp();
-	void ProcessId();
-	void Copy( $x, $y );
-}
-*/
 
 #endif

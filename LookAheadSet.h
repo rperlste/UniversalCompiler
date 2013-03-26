35d758ce93b586169fe6eed1197ed7fc2a05c05d
Robbie Perlstein <rperlste@gmail.com>
2013-03-26 00:00:24 -0600

    Removed zombie code.

--- LookAheadSet.h
+++ LookAheadSet.h
@@ -15,10 +15,6 @@ public:
 	bool setUnion( const Key& key, const Symbol& grammar );
 	bool setUnion( const Key& key, const GrammarSet& grammarSet );
 
-	// Return a Symbol representation of the set
-//	Symbol getSetAsString(const int& index);
-//	Symbol getSetAsString( const Symbol& key );
-
 };
 
 // Return true if change occurred, false if none occurred
@@ -44,116 +40,5 @@ bool LookAheadSet<Key>::setUnion( const Key& key, const GrammarSet& grammarSet )
 		return it->second.setUnion( grammarSet );
 	}
 }
-/*
-// Return a Symbol representation of the set
-Symbol LookAheadSet::getSetAsString(const int& index){
-	return this->get(*getKey( index )) + " = " + getGrammarSet(index)->getSetAsString() + "\n";
-}
 
-Symbol LookAheadSet::getSetAsString( const Symbol& key ){
-	return key + " = " + getGrammarSet(key)->getSetAsString() + "\n";
-}
-
-/*
-bool LookAheadSet::add( std::list<Production> productions ){
-	bool changes = false;
-	for( std::list<Production>::iterator it = productions.begin();
-		it != productions.end();
-		it++ ){
-			for( int i = 0; i < (*it).RHSsize(); i++ ){
-				if( add( (*it).getLHS(), (*it->getRHS()->getSymbol(i)) ))
-					changes = true;
-			}
-	}
-	return changes;
-}
-
-
-// Return true if change occurred, false if none occurred
-bool LookAheadSet::add( const Symbol& key ){
-	if( this->find( key ) == this->end() ){
-		GrammarSet grammarSet;
-		lookAheadSet[ key ] = grammarSet;
-		return true;
-	}
-	else
-		return false;
-}
-*/
-
-/*
-bool LookAheadSet::contains( const Symbol& key, const Symbol& grammar ){
-	std::map< Symbol, GrammarSet >::iterator it = this->find( key );
-	if( it != this->end() )
-		return (*it).second.contains( grammar );
-}
-
-bool LookAheadSet::contains( const int& index, const Symbol& grammar ){
-	return getGrammarSet( index)->contains(grammar);
-}
-
-void LookAheadSet::remove( const Symbol& key, const Symbol& grammar ){
-	if( this->find( key ) != this->end() )
-		this->find( key )->second.remove( grammar );
-}
-
-std::map<Symbol, GrammarSet> LookAheadSet::getLookAheadSet(){
-	return lookAheadSet;
-}
-
-const int LookAheadSet::size(){
-	return this->size();
-}
-
-int LookAheadSet::getIndexOfKey( const Symbol& key ){
-	int count = 0;
-	for( std::map< Symbol, GrammarSet >::iterator it = this->begin();
-		it != this->end();
-		it++ ){
-			if( (*it).first == key )
-				return count;
-			count++;
-	}
-	throw ProductionNotFound( key );
-}
-
-const Symbol* LookAheadSet::getKey( const int& index ){
-	if( index >= size() )
-		throw IndexOutOfBounds();
-	int count = 0;
-	for( std::map< Symbol, GrammarSet >::iterator it = this->begin();
-		it != this->end();
-		it++ ){
-			if( count == index )
-				return &(*it).first;
-			count++;
-	}
-}
-
-GrammarSet* LookAheadSet::getGrammarSet( const int index ){
-	if( index >= size() )
-		throw IndexOutOfBounds();
-	int count = 0;
-	for( std::map< Symbol, GrammarSet >::iterator it = this->begin();
-		it != this->end();
-		it++ ){
-			if( count == index )
-				return &(*it).second;
-			count++;
-	}
-}
-
-GrammarSet* LookAheadSet::getGrammarSet( const Symbol& key ){
-	if( this->find( key ) != this->end() ){
-		return &lookAheadSet[ key ];
-	}
-	else 
-		throw ProductionNotFound( key );
-}
-
-GrammarSet* LookAheadSet::operator [] ( const Symbol& key ){
-	return getGrammarSet( key );
-}
-
-*/
 #endif

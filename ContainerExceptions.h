56b48de76e8c91da02ed0847a9596dc089fd1d1f
Robbie Perlstein <rperlste@gmail.com>
2013-03-25 23:41:55 -0600

    Updated formatting.

--- ContainerExceptions.h
+++ ContainerExceptions.h
@@ -3,9 +3,8 @@
 
 #include <exception>
 
-	class IndexOutOfBounds	: public std::exception {};
-	class KeyNotFound		: public std::exception {};
-	class ValueNotFound		: public std::exception {};
-
+	class IndexOutOfBounds : public std::exception {};
+	class KeyNotFound      : public std::exception {};
+	class ValueNotFound    : public std::exception {};
 
 #endif

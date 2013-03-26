dee0e7f711f2368219d0703a382741975148309e
Robbie Perlstein <rperlste@gmail.com>
2013-03-26 00:05:58 -0600

    Updated formatting.

--- Scanner.h
+++ Scanner.h
@@ -20,58 +20,38 @@
 class Scanner{
 
 public:
-	//////////////////
-	// CONSTRUCTION //
-	//////////////////
+
 	Scanner( std::fstream* );
 
-	///////////////
-	// FUCNTIONS //
-	///////////////
-	Token scannerDriver();
-
-	Token scan();
-
-	Token peekNextToken();
-
-	Token getPreviousToken();
-
-	int getScannerFilePosition() const;
-
-	std::fstream* getProgramFile() const;
-
-	bool isDoneScanning();
-
-	bool hasError();
-
-	void close();
+	Token             scannerDriver();
+	Token             scan();
+	Token             peekNextToken();
+	Token             getPreviousToken();
+	int               getScannerFilePosition() const;
+	std::fstream*     getProgramFile() const;
+	bool              isDoneScanning();
+	bool              hasError();
+	void              close();
 
 private:
-	///////////////
-	// FUCNTIONS //
-	///////////////
-	char getCurrentChar();
-	char peekNextChar();
-	void consumeChar();
 
-	Action::Action getAction( State::State, char );
-	State::State getNextState( State::State, char );
-	TokenEnums lookupCode( State::State, char );
-	void checkExceptions( Token& );
+	char              getCurrentChar();
+	char              peekNextChar();
+	void              consumeChar();
+	Action::Action    getAction( State::State, char );
+	State::State      getNextState( State::State, char );
+	TokenEnums        lookupCode( State::State, char );
+	void              checkExceptions( Token& );
+	void              LexicalError( std::string );
 
-	void LexicalError( std::string );
-
-	///////////////
-	// VARIABLES //
-	///////////////
-	int filePosition;
-	bool errorDetected;
-	bool isClosed;
-	Token* tokenBuffer;
-	Token currentToken;
-	Token previousToken;
-	char* charBuffer;
-	std::fstream* programFile;
+	int               filePosition;
+	bool              errorDetected;
+	bool              isClosed;
+	Token*            tokenBuffer;
+	Token             currentToken;
+	Token             previousToken;
+	char*             charBuffer;
+	std::fstream*     programFile;
 };
 
 #endif
